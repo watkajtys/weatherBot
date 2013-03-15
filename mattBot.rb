f29a85407cc11f8487f4d66a0961d6112faf51f9
Matt Watkajtys <watkajtys@gmail.com>
2013-03-14 23:21:43 -0400

    First build maybe done. Testing.

--- mattBot.rb
+++ mattBot.rb
@@ -10,17 +10,33 @@ class Bot
 		@response_prompt = 'privmsg #bitmaker :'
 		@callings = ['weather', 'temperature', 'current_conditions']
 	end
-end
 
-class Connect
-	attr_reader :server, :port, :channel
-	def initialize
-		@server = 'chat.freenode.net'
-		@port = '6667'
-		@channel = '#bitmaker'
+	def run
+		until server.eof? do 
+			msg = server.gets.downcase
+			puts msg
+
+			wasCalled = false
+			callings.each do |c|
+				wasCalled = true if msg.include? c
+			end
+
+			if msg.include? response_prompt and wasCalled
+				server.puts 'PRIVMSG #{channel}'
+				weather
+			end
+		end
 	end
 
 	def server_connect
 		server = TCPSocket.open(server,port)
+		server.puts "USER WeatherBot 0 * WeatherBot"
+		server.puts "NICK #{nick}"
+		server.puts "JOIN #{channel}"
+		server.puts "PRIVMSG #{channel} :Hello from WeatherBot"
 	end
 end
+
+weatherBot = Bot.new
+weatherBot.server_connect
+weatherBot.run
